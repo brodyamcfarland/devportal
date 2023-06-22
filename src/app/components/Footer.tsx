341309888c97b254b907fec4be5da88ee7dd112b
Brody McFarland <brodyamcfarland@gmail.com>
2023-06-21 18:02:52 -0700

    hid footer on mobile

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
      return (
-          <footer className="flex justify-center items-center py-6 opacity-20 sticky bottom-0 select-none text-xs">
+          <footer className="hidden md:flex justify-center items-center py-6 opacity-20 sticky bottom-0 select-none text-xs">
                Dev Portal &copy; 2023
           </footer>
      );
