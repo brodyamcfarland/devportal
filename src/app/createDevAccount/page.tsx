6203a6d8324b143ca32925064d7da6a97b05dae2
Brody McFarland <brodyamcfarland@gmail.com>
2023-06-27 11:10:23 -0700

    Working on social providers for OAuth, setting up SupabaseAuth

--- src/app/createDevAccount/page.tsx
+++ src/app/createDevAccount/page.tsx
@@ -1,38 +1,10 @@
 import DevAccountForm from "../components/DevAccountForm";
+import Logo from "../components/Logo";
 
 export default function CreateDevAccount() {
      return (
           <main className="flex flex-col md:flex-row mx-auto mainContainer xl:max-w-4xl lg:max-w-3x flex-1 items-center gap-2 p-2">
-               <div className="flex relative mb-5 md:mb-10 mt-5 mx-10">
-                    <div className="h-36 w-36 md:h-64 md:w-64 border-2 border-black dark:border-white rounded-full relative dark:opacity-60 animate-slowspin ">
-                         <div className="absolute h-36 w-36 md:h-64 md:w-64 border right-1 rounded-full animate-slowspin border-red-300 blur-lg" />
-                         <div className="absolute h-36 w-36 md:h-64 md:w-64 border left-1 rounded-full animate-slowspin border-red-500" />
-                         <div className="absolute h-36 w-36 md:h-64 md:w-64 border top-1 rounded-full animate-slowspin border-red-700" />
-                         <div className="absolute h-36 w-36 md:h-64 md:w-64 border bottom-1 rounded-full animate-slowspin border-red-200 blur-lg" />
-                         <div className="absolute h-36 w-36 md:h-64 md:w-64 border-2 rounded-full animate-slowspin border-red-800 blur-sm" />
-                         <div className="absolute h-1 w-1 border-4 rounded-full animate-slowspin dark:border-red-400 border-red blur-sm top-10" />
-                    </div>
-                    <div className="h-32 w-32 md:h-64 md:w-64 rounded-full absolute dark:opacity-70 animate-[spin_20s_linear_infinite]">
-                         <div className="absolute h-1 w-1 border-4 rounded-full dark:border-red-600 border-red-600 blur-sm top-10 left-24 md:top-20 md:left-52 animate-pulse" />
-                    </div>
-                    <div className="h-32 w-32 md:h-64 md:w-64 rounded-full absolute dark:opacity-20 animate-[spin_10s_linear_infinite]">
-                         <div className="absolute h-1 w-1 border-4 rounded-full dark:border-red-600 border-red-600 blur-sm top-10 -left-8 md:top-20 md:-left-10 animate-pulse" />
-                    </div>
-                    <div className="h-32 w-32 md:h-64 md:w-64 rounded-full absolute dark:opacity-100 -z-19 animate-[spin_13s_linear_infinite]">
-                         <div className="absolute h-1 w-1 border-4 rounded-full dark:border-red-600 border-red-600 blur-sm top-10 left-24 md:top-20 md:left-50 animate-pulse" />
-                    </div>
-                    <div className="h-32 w-32 md:h-64 md:w-64 rounded-full absolute dark:opacity-50 -z-19 animate-[spin_15s_linear_infinite]">
-                         <div className="absolute h-1 w-1 border-4 rounded-full dark:border-red-600 border-red-600 blur-sm top-10 left-30 md:top-20 md:left-20 animate-pulse" />
-                    </div>
-                    <div className="absolute flex flex-col items-center justify-center h-36 w-36 md:h-64 md:w-64">
-                         <span className="md:text-3xl dark:opacity-60">
-                              [ dev portal ]
-                         </span>
-                         <span className="text-[10px] md:text-xs dark:opacity-60 text-red-500 font-bold">
-                              DEVELOPER
-                         </span>
-                    </div>
-               </div>
+               <Logo color="red" text="DEVELOPER" />
                <div className="mx-10 flex flex-col flex-1 items-center py-4 h-full overflow-y-auto">
                     <h1 className="dark:opacity-70 tracking-wide pb-2 text-lg font-bold">
                          Create Developer Account
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,137 @@
+"use client";
+
+import { createClient } from "@supabase/supabase-js";
+import { useState } from "react";
+import Logo from "../components/Logo";
+import { TbCircleFilled } from "react-icons/tb";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+// Set up Github and Google Sign in with email sign in as a backup
 const Login = () => {
+     const [email, setEmail] = useState<string>("");
+     const [password, setPassword] = useState<string>("");
+
+     const supabase = createClient(
+          process.env.NEXT_PUBLIC_SUPABASE_URL!,
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+     );
+
+     const signInWithEmail = async () => {
+          const { data, error } = await supabase.auth.signInWithPassword({
+               email: email,
+               password: password,
+          });
+     };
+
+     const signInWithGitHub = async () => {
+          // Need to setup OAuth in Github First []
+          const { data, error } = await supabase.auth.signInWithOAuth({
+               provider: "github",
+          });
+     };
+
+     const signInWithGoogle = async () => {
+          // Need to setup OAuth in Google First []
+          const { data, error } = await supabase.auth.signInWithOAuth({
+               provider: "google",
+          });
+     };
+
      return (
-          <main className="flex flex-col flex-1 items-center justify-center">
-               Login Page
+          <main className="flex flex-col md:flex-row mx-auto mainContainer xl:max-w-4xl lg:max-w-3x flex-1 items-center gap-2 p-2">
+               <Logo color="gray" text="SIGN IN" />
+               <div className="mx-10 flex flex-col flex-1 items-center py-4 h-full overflow-y-auto">
+                    <h1 className="dark:opacity-70 tracking-wide pb-2 text-lg font-bold">
+                         Sign In
+                    </h1>
+                    <section className="flex flex-col w-full gap-2 pb-4">
+                         <button
+                              onClick={signInWithGoogle}
+                              className="relative w-full inline-flex items-center justify-center p-0.5 overflow-hidden text-sm text-gray-900 rounded-lg group bg-gradient-to-br from-yellow-800 via-blue-800 to-red-900 group-hover:to-blue-500 hover:text-white dark:text-white"
+                         >
+                              <span className="flex gap-2 items-center justify-center text-center w-full relative px-5 py-2.5 transition-all ease-in duration-100 bg-white dark:bg-black rounded-md group-hover:bg-opacity-0 bg-gradient-to-br from-gray-800/20 to-red-900/20">
+                                   <FcGoogle size={18} />
+                                   Google Sign In
+                              </span>
+                         </button>
+                         <button
+                              onClick={signInWithGitHub}
+                              className="relative w-full inline-flex items-center justify-center p-0.5 overflow-hidden text-sm text-gray-900 rounded-lg group bg-gradient-to-br from-gray-800 to-purple-900 group-hover:to-purple-500 hover:text-white dark:text-white"
+                         >
+                              <span className="flex gap-2 items-center justify-center text-center w-full relative px-5 py-2.5 transition-all ease-in duration-100 bg-white dark:bg-black rounded-md group-hover:bg-opacity-0 bg-gradient-to-br from-gray-800/20 to-purple-900/20">
+                                   <AiFillGithub size={18} />
+                                   Github Sign In
+                              </span>
+                         </button>
+                    </section>
+                    <form
+                         onSubmit={signInWithEmail}
+                         className="flex flex-col gap-2 w-full"
+                    >
+                         <label className="flex flex-col">
+                              <div className="flex flex-row gap-2 items-center text-left text-sm">
+                                   <span className="pl-2 opacity-80">
+                                        Email Address
+                                   </span>
+                                   {email ? (
+                                        <TbCircleFilled
+                                             size={12}
+                                             className="h-3 w-3 text-green-500/70"
+                                        />
+                                   ) : (
+                                        <TbCircleFilled
+                                             size={12}
+                                             className="h-3 w-3 text-red-500/70"
+                                        />
+                                   )}
+                              </div>
+                              <input
+                                   type="text"
+                                   value={email}
+                                   onChange={(e) => setEmail(e.target.value)}
+                                   className="w-full p-2 rounded-md"
+                                   required
+                                   placeholder="johndoe@gmail.com"
+                                   title="Email Address"
+                              />
+                         </label>
+
+                         <label className="flex flex-col">
+                              <div className="flex flex-row gap-2 items-center text-left text-sm">
+                                   <span className="pl-2 opacity-80">
+                                        Password
+                                   </span>
+                                   {password ? (
+                                        <TbCircleFilled
+                                             size={12}
+                                             className="h-3 w-3 text-green-500/70"
+                                        />
+                                   ) : (
+                                        <TbCircleFilled
+                                             size={12}
+                                             className="h-3 w-3 text-red-500/70"
+                                        />
+                                   )}
+                              </div>
+                              <input
+                                   type="password"
+                                   value={password}
+                                   onChange={(e) => setPassword(e.target.value)}
+                                   className="w-full p-2 rounded-md"
+                                   required
+                                   title="Set Password"
+                              />
+                         </label>
+                         <button
+                              type="submit"
+                              className="relative w-full inline-flex mt-4 items-center justify-center p-0.5 overflow-hidden text-sm text-gray-900 rounded-lg group bg-gradient-to-br from-gray-800 to-red-900 group-hover:to-blue-500 hover:text-white dark:text-white"
+                         >
+                              <span className="text-center w-full relative px-5 py-2.5 transition-all ease-in duration-100 bg-white dark:bg-black rounded-md group-hover:bg-opacity-0 bg-gradient-to-br from-gray-800/20 to-red-900/20">
+                                   Sign In
+                              </span>
+                         </button>
+                    </form>
+               </div>
           </main>
      );
 };
